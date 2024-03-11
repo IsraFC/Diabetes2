054da9475cdc5039d456367c163de2e70a671f21
ISRAEL FERNANDEZ CARRERA <israel.fernadez@madero.edu.mx>
2024-03-10 21:56:29 -0600

    Actualizacion 3

--- Diabetes2/Data/DataContext.cs
+++ Diabetes2/Data/DataContext.cs
@@ -1,6 +1,28 @@
-﻿namespace Diabetes2.Data
+﻿using Diabetes2.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Diabetes2.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<GlucoseMonitoring> GlucoseMonitorings { get; set; }
+        public DbSet<HealthcareP> HealthcarePs { get; set; }
+        public DbSet<MealPlanning> MealPlannings { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Progress> Progresses { get; set; }
+        public DbSet<ReminderAlert> reminderAlerts { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
