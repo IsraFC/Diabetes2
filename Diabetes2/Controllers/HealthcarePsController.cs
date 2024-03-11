eccf362d7372fafa9e1b1e3f757969065c6d8a18
ISRAEL FERNANDEZ CARRERA <israel.fernadez@madero.edu.mx>
2024-03-11 08:52:47 -0600

    Actualizacion 7

---
+++ Diabetes2/Controllers/HealthcarePsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Diabetes2.Data;
+using Diabetes2.Data.Entities;
+
+namespace Diabetes2.Controllers
+{
+    public class HealthcarePsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public HealthcarePsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: HealthcarePs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.HealthcarePs.ToListAsync());
+        }
+
+        // GET: HealthcarePs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var healthcareP = await _context.HealthcarePs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (healthcareP == null)
+            {
+                return NotFound();
+            }
+
+            return View(healthcareP);
+        }
+
+        // GET: HealthcarePs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HealthcarePs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Description,Name,profession")] HealthcareP healthcareP)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(healthcareP);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(healthcareP);
+        }
+
+        // GET: HealthcarePs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var healthcareP = await _context.HealthcarePs.FindAsync(id);
+            if (healthcareP == null)
+            {
+                return NotFound();
+            }
+            return View(healthcareP);
+        }
+
+        // POST: HealthcarePs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Description,Name,profession")] HealthcareP healthcareP)
+        {
+            if (id != healthcareP.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(healthcareP);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HealthcarePExists(healthcareP.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(healthcareP);
+        }
+
+        // GET: HealthcarePs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var healthcareP = await _context.HealthcarePs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (healthcareP == null)
+            {
+                return NotFound();
+            }
+
+            return View(healthcareP);
+        }
+
+        // POST: HealthcarePs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var healthcareP = await _context.HealthcarePs.FindAsync(id);
+            if (healthcareP != null)
+            {
+                _context.HealthcarePs.Remove(healthcareP);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HealthcarePExists(int id)
+        {
+            return _context.HealthcarePs.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Diabetes2/Controllers/ProgressesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Diabetes2.Data;
+using Diabetes2.Data.Entities;
+
+namespace Diabetes2.Controllers
+{
+    public class ProgressesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ProgressesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Progresses
+        public async Task<IActionResult> Index()
+        {
+            var dataContext = _context.Progresses.Include(p => p.Patient).Include(p => p.User);
+            return View(await dataContext.ToListAsync());
+        }
+
+        // GET: Progresses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var progress = await _context.Progresses
+                .Include(p => p.Patient)
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (progress == null)
+            {
+                return NotFound();
+            }
+
+            return View(progress);
+        }
+
+        // GET: Progresses/Create
+        public IActionResult Create()
+        {
+            ViewData["Id"] = new SelectList(_context.Patients, "Id", "Id");
+            ViewData["Id"] = new SelectList(_context.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Progresses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Graphics,HealthStadistics")] Progress progress)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(progress);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Id"] = new SelectList(_context.Patients, "Id", "Id", progress.Id);
+            ViewData["Id"] = new SelectList(_context.Users, "Id", "Email", progress.Id);
+            return View(progress);
+        }
+
+        // GET: Progresses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var progress = await _context.Progresses.FindAsync(id);
+            if (progress == null)
+            {
+                return NotFound();
+            }
+            ViewData["Id"] = new SelectList(_context.Patients, "Id", "Id", progress.Id);
+            ViewData["Id"] = new SelectList(_context.Users, "Id", "Email", progress.Id);
+            return View(progress);
+        }
+
+        // POST: Progresses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Graphics,HealthStadistics")] Progress progress)
+        {
+            if (id != progress.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(progress);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProgressExists(progress.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Id"] = new SelectList(_context.Patients, "Id", "Id", progress.Id);
+            ViewData["Id"] = new SelectList(_context.Users, "Id", "Email", progress.Id);
+            return View(progress);
+        }
+
+        // GET: Progresses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var progress = await _context.Progresses
+                .Include(p => p.Patient)
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (progress == null)
+            {
+                return NotFound();
+            }
+
+            return View(progress);
+        }
+
+        // POST: Progresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var progress = await _context.Progresses.FindAsync(id);
+            if (progress != null)
+            {
+                _context.Progresses.Remove(progress);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProgressExists(int id)
+        {
+            return _context.Progresses.Any(e => e.Id == id);
+        }
+    }
+}
